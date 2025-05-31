a43542d03ec036b467e6dc132dff1d306b1a7f00
Paulo César Lins <paulocsar91@gmail.com>
2025-05-31 17:44:08 -0300

    Adiciona navegação.

--- src/app/add/index.tsx
+++ src/app/add/index.tsx
@@ -2,12 +2,14 @@ import { View, Text, TouchableOpacity } from "react-native"
 import { MaterialIcons } from "@expo/vector-icons"
 import { styles } from "./style"
 import { colors } from "@/styles/colors"
+import { router } from "expo-router"
+import { Categories } from "@/components/categories"
 
 export default function Add(){
     return(
         <View style={styles.container} >
             <View style={styles.header} >
-                <TouchableOpacity>
+                <TouchableOpacity onPress={() => router.back()} >
                     <MaterialIcons name={"arrow-back"} size={32} color={colors.gray[200]} />
                 </TouchableOpacity>
 
@@ -15,6 +17,8 @@ export default function Add(){
             </View>
 
             <Text style={styles.label} >Selecione uma categoria</Text>
+
+            <Categories />
         </View>
     )
 }
