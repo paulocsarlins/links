1e51331ace156680715280447606a371a183c1b6
Paulo César Lins <paulocsar91@gmail.com>
2025-05-31 17:24:53 -0300

    Cria tela para adicionar novos links.

---
+++ src/app/add/index.tsx
@@ -0,0 +1,20 @@
+import { View, Text, TouchableOpacity } from "react-native"
+import { MaterialIcons } from "@expo/vector-icons"
+import { styles } from "./style"
+import { colors } from "@/styles/colors"
+
+export default function Add(){
+    return(
+        <View style={styles.container} >
+            <View style={styles.header} >
+                <TouchableOpacity>
+                    <MaterialIcons name={"arrow-back"} size={32} color={colors.gray[200]} />
+                </TouchableOpacity>
+
+                <Text style={styles.title} >Novo</Text>
+            </View>
+
+            <Text style={styles.label} >Selecione uma categoria</Text>
+        </View>
+    )
+}
